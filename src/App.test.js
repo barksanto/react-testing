f150883cb5ebc3ad76b6f1f4efa1be5f569105e1
Barkley <barkley.santo@gmail.com>
2022-12-29 19:01:20 +0800

    clarify basic test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,11 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react"
+import App from "./App"
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("renders learn react link", () => {
+  // must render component, otherise it will not be in the document
+  render(<App />)
+  // select this link element by text
+  const linkElement = screen.getByText(/learn react/i)
+  // assert that the link element is in the document
+  expect(linkElement).toBeInTheDocument()
+})

e1f8dcd623607cbb5966bfc1ede78c43b6cce8ed
Barkley <barkley.santo@gmail.com>
2022-12-29 19:03:29 +0800

    choosing element by testid instead of text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg"
+import "./App.css"
 
 function App() {
   return (
@@ -10,6 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
+          data-testid="learn-react-link"
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
@@ -19,7 +20,7 @@ function App() {
         </a>
       </header>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/App.test.js
+++ src/App.test.js
@@ -4,8 +4,11 @@ import App from "./App"
 test("renders learn react link", () => {
   // must render component, otherise it will not be in the document
   render(<App />)
-  // select this link element by text
-  const linkElement = screen.getByText(/learn react/i)
+  // @ select this link element by its text
+  // const linkElement = screen.getByText(/learn react/i)
+
+  // @ select this link element by its data-testid (does same as above)
+  const linkElement = screen.getByTestId("learn-react-link")
   // assert that the link element is in the document
   expect(linkElement).toBeInTheDocument()
 })

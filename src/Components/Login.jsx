8a09896d9545df046219f88596e02b12b199da25
Barkley <barkley.santo@gmail.com>
2023-01-06 13:29:25 +0800

    async server call to json placeholder setting user state after login

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,13 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+// import axios from 'axios'
 
 function Login() {
-  return (
-    <div className="container">
-      <form>
+  const [error, setError] = useState(false)
+  const [username, setUsername] = useState("")
+  const [password, setPassword] = useState("")
+  const [user, setUser] = useState({})
 
-        <input type="text" placeholder='username'/>
-        <input type="curent-password"/>
-        <button>Login</button>
+  const handleClick = async (e) => {
+    e.preventDefault()
+
+    try {
+      // const { data } = axios.get('https://jsonplaceholder.typicode.com/users/1')
+      // setUser(data)
+      fetch('https://jsonplaceholder.typicode.com/users/1')
+        .then(response => response.json())
+        .then(data => setUser(data))
+        .then(() => console.log(user))
+
+
+
+    } catch (error) {
+      console.log(error)
+      setError(true)
+    }
+
+  }
+
+  
+  return (
+
+    <div className="container"> 
+      <span>User: {user.name }</span>
+      <form>
+        <input type="text" placeholder='username' value={username} onChange={e=> setUsername(e.target.value) } />
+        <input type="curent-password" placeholder='password' value={password} onChange={e => setPassword(e.target.value)} />
+        <button disabled={!username || !password} onClick={handleClick}>Login</button>
+        <span style={{visibility: error ? "visible" : "hidden"}}>Something went wrong.</span>
       </form>
     </div>
   )
--- src/Components/Login.test.jsx
+++ src/Components/Login.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react"
+import { fireEvent, render, screen } from "@testing-library/react"
 import Login from "./Login"
 
 describe("Login", () => { 
@@ -8,10 +8,66 @@ describe("Login", () => {
   })
 
   it("username input is rendered", () => {
-      render(<Login />)
-    expect(screen.getByPlaceholderText("username")).toBeInTheDocument()
+    render(<Login />)
+    const usernameInput = screen.getByPlaceholderText("username")
+    expect(usernameInput).toBeInTheDocument()
   })
 
+  it('renders enter button', () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    expect(loginButton).toBeInTheDocument()
+  })
+
+   it("username input is should be empty", () => {
+    render(<Login />)
+    const usernameInput = screen.getByPlaceholderText("username")
+    expect(usernameInput.value).toBe("")
+  })
+
+  it('button is disabled if inputs arent filled', () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    expect(loginButton).toBeDisabled()
+  })
+
+  it('error message should not be visible', () => { 
+    render(<Login />)
+    const errorMessage = screen.getByText("Something went wrong.")
+    expect(errorMessage).not.toBeVisible()
+  })
+
+  it("username input should change", () => {
+    render(<Login />)
+    const usernameInputEl = screen.getByPlaceholderText("username")
+
+    const testValue = "test"
+    fireEvent.change(usernameInputEl, { target: { value: testValue } })
+    expect(usernameInputEl.value).toBe(testValue)
+  })
+
+   it("password input should change", () => {
+     render(<Login />)
+     const passwordInputEl = screen.getByPlaceholderText("password")
+     const testValue = "test"
+     fireEvent.change(passwordInputEl, { target: { value: testValue } })
+     expect(passwordInputEl.value).toBe(testValue)
+   })
   
+   it('button should not be disabled when inputs exist', () => {
+    render(<Login />)
+    const loginButton = screen.getByRole("button")
+    const usernameInput = screen.getByPlaceholderText("username")
+    const passwordInputEl = screen.getByPlaceholderText("password")
+
+    const fakeTestValue = "test"
+    
+    fireEvent.change(usernameInput, { target: { value: fakeTestValue } })
+    fireEvent.change(passwordInputEl, { target: { value: fakeTestValue } })
+    //  const fakePassword = "test123"
+    expect(loginButton).not.toBeDisabled()
+  })
+
+
 })
 
